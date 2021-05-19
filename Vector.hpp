1f4c33f5f3f2d5fdc5f06fbccb0c2c58bcaf2bda
Choyeon <chyajt@gmail.com>
2021-05-19 22:33:58 +0900

    Feat: Vector3D

--- Vector.hpp
+++ Vector.hpp
@@ -36,6 +36,14 @@ namespace chymath
 			return *this;
 		}
 
+		Vector2D& operator/=(double scalar)
+		{
+			X /= scalar;
+			Y /= scalar;
+
+			return *this;
+		}
+
 		double X;
 		double Y;
 	};
@@ -63,5 +71,94 @@ namespace chymath
 	double operator*(const Vector2D& lhs, const Vector2D& rhs)
 	{
 		return lhs.X * rhs.X + lhs.Y * rhs.Y;
+	}
+
+	Vector2D operator/(const Vector2D& lhs, double scalar)
+	{
+		return Vector2D(lhs.X / scalar, lhs.Y / scalar);
+	}
+
+	Vector2D operator/(double scalar, const Vector2D& rhs)
+	{
+		return rhs * scalar;
+	}
+
+	class Vector3D
+	{
+	public:
+		Vector3D(double x = 0.f, double y = 0.f, double z = 0.f) : X(x), Y(y), Z(z) {}
+
+		double Size() const { return sqrt(X * X + Y * Y + Z * Z); }
+		void Normalize() { X /= Size(); Y /= Size(); Z /= Size(); }
+
+		Vector3D& operator+=(const Vector3D& rhs)
+		{
+			X += rhs.X;
+			Y += rhs.Y;
+			Z += rhs.Z;
+
+			return *this;
+		}
+
+		Vector3D& operator-=(const Vector3D& rhs)
+		{
+			X -= rhs.X;
+			Y -= rhs.Y;
+			Z -= rhs.Z;
+
+			return *this;
+		}
+
+		Vector3D& operator*=(double scalar)
+		{
+			X *= scalar;
+			Y *= scalar;
+			Z *= scalar;
+
+			return *this;
+		}
+
+		Vector3D& operator/=(double scalar)
+		{
+			X /= scalar;
+			Y /= scalar;
+			Z /= scalar;
+
+			return *this;
+		}
+
+		double X;
+		double Y;
+		double Z;
+	};
+
+	Vector3D operator+(const Vector3D& lhs, const Vector3D& rhs)
+	{
+		return Vector3D(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z);
+	}
+
+	Vector3D operator-(const Vector3D& lhs, const Vector3D& rhs)
+	{
+		return Vector3D(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z);
+	}
+
+	Vector3D operator*(const Vector3D& lhs, double scalar)
+	{
+		return Vector3D(lhs.X * scalar, lhs.Y * scalar, lhs.Z * scalar);
+	}
+
+	Vector3D operator*(double scalar, const Vector3D& rhs)
+	{
+		return rhs * scalar;
+	}
+
+	Vector3D operator/(const Vector3D& lhs, double scalar)
+	{
+		return Vector3D(lhs.X / scalar, lhs.Y / scalar, lhs.Z / scalar);
+	}
+
+	Vector3D operator/(double scalar, const Vector3D& rhs)
+	{
+		return rhs / scalar;
 	}
 }
